927ecd6454697d5c90ddddb996883f1b15857643
Fadıl Burak Yurtsever <fadil.13urak.yurtsever@gmail.com>
2021-02-24 22:12:24 +0300

    Advanced Kotlin Project 1 - "Navigation"
    Not : Android için önemli 4 faktör :
    1) Aktiviteler :
    2) Servisler : Genelde arka planda çalıştırdığımız. Arka planda yapılması gereken işlemleri kullanıcının haberi olmadan yaptırdığımız kısımdır. Örneğin internetten veri indirilmesi.
    3) Broadcast Receivers Yayın alıcısı : Özellikle uygulamarın birbiriyle haberleşmesini ve uygulama içerisinde haberleşmeleri yöneten kısımdır.
    4) Content Providers İcerik Saglayıcıları : Bir uygulamadan baska bir uygulamaya veya aynı uygulama icerisinde bir veri saglanacak ise ve bu veriyi daha iyi elde edebilmek icin yazdıgımız bilesendir.
    
    Not : Navigasyonun bize sundugu en büyük özellik Intent ile yaptığımız bir çok işlemi daha kolay yapabilmemiz ve veri iletişimi sıkıntısız aktarabilmemizdir.
    Not : Navigasyon , kullanıcıların aktiviteler arasında ve fragmanlar arasındaki gecisleri daha iyi yönetebilmek icin olusturulan bir kütüphanedir.
    Not : layout tagi bulunduğu framlayoutun bir navigasyon öğresi olmasını sağlar.
    Not : Navigasyon grafiği uygulama iceriğinde neler varsa fragman veya aktivite bunların birbiryle nasıl baglantılı oldugunu ve bu baglantıların nasıl yönetilmesi gerektigini belirten grafiktir.

---
+++ KotlinNavigation2/app/src/main/java/com/fburaky/kotlinnavigation/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.fburaky.kotlinnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navigationController : NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navigationController = Navigation.findNavController(this , R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this , navigationController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        // Üstte bulunan geri dönme barını eklemek için aşağıdaki kodumuzu yazıyoruz .
+        return NavigationUI.navigateUp(navigationController ,null)
+    }
+}
